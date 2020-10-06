6a9f4c669636469de17093c87fac56916f3972c8
Maita Tomoya <58167405+mt114ran@users.noreply.github.com>
2020-10-06 23:09:22 +0900

    add NumberOfRoutes.c

---
+++ NumberOfRoutes.c
@@ -0,0 +1,50 @@
+/*
+ * task2_NumberOfRoutes.c
+ *
+ *  Created on: 2020/05/25
+ *      Author: 新宿MBD研修9
+ */
+
+
+#include <stdio.h>
+
+int Factorial(int);
+
+int main(void){
+
+	int x;
+	int y;
+	int Ans;
+
+	setvbuf(stdout,NULL,_IONBF,0);
+
+	printf("終点のX座標を入力して下さい\n");
+	scanf("%d",&x);
+
+	printf("終点のY座標を入力して下さい\n");
+	scanf("%d",&y);
+
+	Ans = Factorial(x+y)/(Factorial(x)*Factorial(y));
+
+	printf("始点（0,0）から終点（%d,%d）までの経路の総数は %d です。\n",x,y,Ans);
+
+	return 0;
+}
+
+
+int Factorial(int x){
+	int Ans;
+
+	if(x>0){
+		Ans = x * Factorial(x-1);
+		return Ans;
+	}
+	else{
+		return 1;
+	}
+
+
+}
+
+
+
